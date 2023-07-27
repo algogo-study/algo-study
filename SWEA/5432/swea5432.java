2a03cd367ac439c971f5c31be11846b61047d083
hyejinKim1 <201802084@o.cnu.ac.kr>
2023-07-27 17:53:52 +0900

    [SWEA 5432] 쇠막대기 자르기

---
+++ SWEA/5432/swea5432.java
@@ -0,0 +1,29 @@
+package ssafy.algorithm.week4;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class swea5432 {
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(new FileInputStream("input5432.txt"));
+		int t = sc.nextInt();
+		
+		for(int test_case = 1; test_case <= t; test_case++) {
+            int res = 0;
+            int cnt = 0;
+            
+            String line = sc.next();
+            for(int i = 0; i < line.length() - 1; i++) {
+                if(line.charAt(i) == '(' && line.charAt(i + 1) == ')') { 
+                    res += cnt;
+                    i++;
+                }
+                else if(line.charAt(i) == '(') cnt++; // 막대 추가
+                else { cnt--; res++; } // 막대 끝
+            }
+            System.out.println("#" + t + " " + ++res);
+		}	
+	}
+}
