3b99f72ed79980cec257bd6d533f6037007c5a47
hyejinKim1 <201802084@o.cnu.ac.kr>
2023-08-03 17:57:42 +0900

    [BOJ 17135] 캐슬 디펜스

--- BOJ/17135/hyejinKim1/BOJ17135_캐슬디펜스.java
+++ BOJ/17135/hyejinKim1/BOJ17135_캐슬디펜스.java
@@ -5,17 +5,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class BOJ17135_캐슬디펜스 {
-	// 완성 못함
+public class BOJ17135_캐슬디펜스{
 	static int n, m, d, num, cnt;
 	static int[][] enemy;
-	static int[][] check;
 	static int[] col_num;
-	
-	static int[] dy = { 0, -1, 0 };
-	static int[] dx = { -1, 0, 1 };
-	
-	static List <Integer> li = new ArrayList();
+
+	static int[] dy = {0, 0, -1, 0 };
+	static int[] dx = {0, -1, 0, 1 };
+
+	static List<Integer> li = new ArrayList();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,8 +22,7 @@ public class BOJ17135_캐슬디펜스 {
 		m = sc.nextInt();
 		d = sc.nextInt();
 
-		enemy = new int[n][m];
-		check = new int[n][m];
+		enemy = new int[n + 1][m];
 		col_num = new int[m];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
@@ -36,77 +33,75 @@ public class BOJ17135_캐슬디펜스 {
 				}
 			}
 		}
-		
-		if(d == 1) {
+
+		int answer = 0;
+
+		if (d == 1) { // 바로 앞에만 보기 때문에 열의 합 가장 큰 3개
 			Arrays.sort(col_num);
-			for(int i = 1; i <= 3; i++) {
-				cnt += col_num[m-i];
+			for (int i = 1; i <= 3; i++) {
+				answer += col_num[m - i];
 			}
-			System.out.println(cnt);
-		}else {
-			// 성 5칸 중에 3개 고르기 - 조합
-			recursive(new int[] { 0, 1, 2, 3, 4 }, new int[3], 0, 0);
-			
-			int answer = 0;
-			
-			for(int i=0; i<li.size(); i++) {
+		} else {
+
+			int[] arr = new int[m];
+			for (int i = 0; i < arr.length; i++) {
+
+			}
+
+			recursive(new int[3], 0, 0);
+
+			for (int i = 0; i < li.size(); i++) { // 모든 조합 중에 최대 값 고르기
 				answer = Math.max(answer, li.get(i));
 			}
-			System.out.println(answer);
 		}
+
+		System.out.println(answer);
 	}
 
-	//3명의 궁수 위치 구하기 - 조합
-	private static void recursive(int[] arr, int[] sel, int idx, int k) {
+	// 3명의 궁수 위치 구하기 - 조합
+	private static void recursive(int[] sel, int idx, int k) {
 		// basis part
-		//한 턴  끝나면 -1 되돌리기
-		// d가 1 이상일때만 이므로 
 		if (k == sel.length) {
+			boolean[][] kill = new boolean[n + 1][m];
 			cnt = 0;
-			for(int y = n-1; y>=0; y--) {
-				for(int sel_idx : sel) {
-					//적이 있고 이번 턴에 다른 궁수가 쏘지 않았으면 
-					if(enemy[y][sel_idx]==1 && check[y][sel_idx] == 0) { 
-						check[y][sel_idx] = 1;
-						cnt++;
-					}
-					else if( enemy[y][sel_idx] == 0 && check[y][sel_idx] == 0) { // 0이면 depth만큼 탐색
-						search(y, sel_idx, 1);
-					}
+			for (int y = n; y > 0; y--) {
+				for (int sel_idx : sel) {
+					search(kill, y, sel_idx, 0);
 				}
 			}
 			li.add(cnt);
 			return;
 		}
 		// inductive part
-		for (int i = idx; i < arr.length; i++) {
-			sel[k] = arr[i];
-			recursive(arr, sel, i + 1, k + 1);
+		for (int i = idx; i < m; i++) {
+			sel[k] = i;
+			recursive(sel, i + 1, k + 1);
 		}
 	}
 	
-	private static void search(int y, int x, int k) {
+	//현재- 좌 - 상 - 우 순서로 탐색
+	private static void search(boolean[][] kill, int y, int x, int k) {
 		if (k == d)
 			return;
 		if (cnt == num)
 			return;
 
-		for(int i=0; i<3; i++){
+		for (int i = 0; i < 4; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			
+
 			if (ny >= 0 && nx >= 0 && nx < m) {
-				if (enemy[ny][nx] == 1  && check[ny][nx] == 0) {
-					check[ny][nx] = 1;
+				if (enemy[ny][nx] == 1 && !kill[ny][nx]) {
+					kill[ny][nx] = true;
 					cnt++;
+//					System.out.println("search" + ny+ nx);
 					return;
-				}else if(enemy[ny][nx] == 1  && check[ny][nx] == 1) {
+				} else if (kill[ny][nx]) // 다른 궁수와 같은 적 쏜 경우
 					return;
-				}
-				else if (enemy[ny][nx] == 0 && check[ny][nx] == 0)
-					search(ny, nx, k + 1);
+				else if (enemy[ny][nx] == 0)
+					search(kill, ny, nx, k + 1);
 			}
 		}
 	}
-	
+
 }
