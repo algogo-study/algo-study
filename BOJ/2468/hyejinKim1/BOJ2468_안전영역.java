e2964e24fdea5a19fe5701c35417730fa84726ef
hyejinKim1 <201802084@o.cnu.ac.kr>
2023-08-03 11:05:02 +0900

    [BOJ 2468] 안전 영역 수정

--- BOJ/2468/hyejinKim1/BOJ2468_안전영역.java
+++ BOJ/2468/hyejinKim1/BOJ2468_안전영역.java
@@ -2,7 +2,7 @@ package ssafy.algorithm.week5;
 
 import java.util.Scanner;
 
-public class BOJ2468_안전영역 {
+public class Main {
 	static int[][] arr;
 	static boolean[][] v;
 	static int n;
@@ -14,7 +14,6 @@ public class BOJ2468_안전영역 {
 		Scanner sc = new Scanner(System.in);
 
 		n = sc.nextInt();
-
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
 
@@ -23,12 +22,8 @@ public class BOJ2468_안전영역 {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				arr[i][j] = sc.nextInt();
-				if (arr[i][j] < min) {
-					min = arr[i][j];
-				}
-				if (arr[i][j] > max) {
-					max = arr[i][j];
-				}
+				min = Math.min(min, arr[i][j]);
+				max = Math.max(max, arr[i][j]);
 			}
 		}
 
@@ -36,13 +31,14 @@ public class BOJ2468_안전영역 {
 
 		// 최저 높이에서 최대 높이까지 반복
 		// 경계값보다 높은 값이 있으면 이어진 곳 탐색하면서 visit 배열에 true
-		for (int height = 0; height < max + 1; height++) {
+		for (int height = min - 1; height < max; height++) {
 			v = new boolean[n][n];
 			int cnt = 0;
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
 					if (!v[i][j] && arr[i][j] > height) {
-						cnt += dfs(i, j, height);
+						cnt++;
+						dfs(i, j, height);
 					}
 
 				}
@@ -53,7 +49,7 @@ public class BOJ2468_안전영역 {
 	}
 
 	// 안전 영역 탐색
-	static int dfs(int x, int y, int height) {
+	static void dfs(int x, int y, int height) {
 		v[x][y] = true;
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
@@ -67,6 +63,6 @@ public class BOJ2468_안전영역 {
 				dfs(nx, ny, height);
 			}
 		}
-		return 1;
+		return;
 	}
 }
