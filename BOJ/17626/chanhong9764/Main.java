309905346c8df476f838c6339e5e3d40f8c1cfec
박찬홍 <chanhong9784@naver.com>
2023-08-03 10:58:36 +0900

    [BOJ 17626] Four Squares

---
+++ BOJ/17626/chanhong9764/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int dp[];
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		dp = new int[n + 1];
+		
+		dp[1] = 1;
+		
+		for(int i = 2; i <= n; i++) {
+			int min_value = Integer.MAX_VALUE;
+			for(int j = 1; j <= Math.sqrt(i); j++) {
+				min_value = Math.min(min_value, dp[i - j*j]);
+			}
+			dp[i] = min_value + 1;
+		}
+		System.out.println(dp[n]);
+	}
+}
