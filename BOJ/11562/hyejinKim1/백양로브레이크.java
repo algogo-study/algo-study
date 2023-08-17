6213e9bb67e871d87ebcb932a42d14538dbfd7d4
hyejinKim1 <201802084@o.cnu.ac.kr>
2023-08-17 17:53:04 +0900

    [BOJ 11562] 백양로 브레이크

--- BOJ/11562/hyejinKim1/백양로브레이크.java
+++ BOJ/11562/hyejinKim1/백양로브레이크.java
@@ -4,10 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//17퍼 실패
-
 public class 백양로브레이크 {
-	static int [][] building;
+	static class Node {
+		int s, dis;
+
+		public Node(int start, int dis) {
+			super();
+			this.s = start;
+			this.dis = dis;
+		}
+	}
+
+	static int n, m, MAX = 100000;
+	static int[][] building, dist;
+	static boolean[] v;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -15,15 +25,24 @@ public class 백양로브레이크 {
 
 		String[] split = br.readLine().split(" ");
 
-		int n = Integer.parseInt(split[0]);
-		int m = Integer.parseInt(split[1]);
+		n = Integer.parseInt(split[0]); // 건물의 수
+		m = Integer.parseInt(split[1]); // 길의 수
 
-		building = new int[251][251];
-		
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
-				if(i == j) building[i][j]=0;
-				else building[i][j] = 251;
+		building = new int[n + 1][n + 1];
+		dist = new int[n + 1][n + 1];
+
+		for (int i = 1; i < n + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				
+				if (i == j) {
+					dist[i][j] = 0;
+					building[i][j] = 0; // 길이 존재하면 0
+				}
+				else {
+					dist[i][j] = MAX;
+					building[i][j] = MAX;
+				}
+					
 			}
 		}
 
@@ -32,31 +51,49 @@ public class 백양로브레이크 {
 			int u = Integer.parseInt(split[0]);
 			int v = Integer.parseInt(split[1]);
 			int b = Integer.parseInt(split[2]);
-			
+
 			building[u][v] = 0;
-			
-			if(b==1) building[v][u] = 0;
-			else building[v][u] = 1;
+
+			if (b == 1)
+				building[v][u] = 0;
+			else
+				building[v][u] = 1; // v-u로 가려면 양방향으로 바꿔줘야 함
 		}
-		
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
-				for(int k=1; k<=n; k++) {
-					if(building[i][k] > building[i][j]+building[j][k])
-						building[i][k] = building[i][j]+building[j][k];
-				}
-			}
+
+		for (int i = 1; i < n + 1; i++) {
+			bfs(i);
 		}
-			
+
 		int k = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < k; i++) {
 			split = br.readLine().split(" ");
 			int s = Integer.parseInt(split[0]);
 			int e = Integer.parseInt(split[1]);
-			
-			System.out.println(building[s][e]);
+
+			System.out.println(dist[s][e]);
 		}
-			
+
+	}
+
+	private static void bfs(int k) {
+		v = new boolean[n + 1];
+		PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> {
+			return Integer.compare(o1.dis, o2.dis);
+		});
+		q.offer(new Node(k, 0));
+
+		while (!q.isEmpty()) {
+			Node p = q.poll();
+
+			dist[k][p.s] = Math.min(dist[k][p.s], p.dis);
+			if (!v[p.s]) {
+				v[p.s] = true;
+				for (int i = 1; i < n + 1; i++) {
+					if(building[p.s][i] != MAX)
+						q.add(new Node(i, building[p.s][i]+p.dis));
+				}
+			}
+		}
 	}
 }
